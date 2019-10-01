a5678da69ebd0edbe8c0878cdcaeba94b34cfd76
KushwahaVaibhav <vaibhab2015@gmail.com>
2019-10-01 23:38:14 +0530

    ninth commit

---
+++ prpy.py
@@ -0,0 +1,15 @@
+x=int(input("enter number of digits to print = "))
+a=0
+b=1
+c=x-2
+i=0
+print(a)
+if(x>1):
+    print(b)
+while(i<c):
+    d=a+b
+    a=b
+    b=d
+    print(d)
+    i=i+1
+      
